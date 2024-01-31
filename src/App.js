d81c61e5950030a6a773c90de41af3280e02b170
강계령 <kearung.kang@gmail.com>
2024-01-31 17:45:59 +0900

    챕터1 완료

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import "./App.css"
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
-export default class App extends Component{
-  render() {
+const App = () => {
+
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, charge: '콜라', amount: 2000 },
+    { id: 2, charge: '빵', amount: 1000 },
+    { id: 3, charge: '맥북', amount: 20000 }
+  ])
+
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState(0)
+
+  const handleCharge = (e) => {
+    setCharge(e.target.value);
+  }
+
+  const handleAmount = (e) => {
+    setAmount(e.target.valueAsNumber);
+  }
+
+  const handleDelete = (id) => {
+    const newExpense = expenses.filter(expense => expense.id !== id)
+    setExpenses(newExpense)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      const newExpense = { id: crypto.randomUUID(), charge, amount }
+      
+      const newExpenses = [...expenses, newExpense];
+
+      setExpenses(newExpenses);
+      setCharge("");
+      setAmount(0);
+    } else {
+      console.error('error');
+    }
+  }
+
     return (
       <main className="main-container">
         <div className="sub-container">
           <h1>장바구니</h1>
 
           <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-            {/* Expense Form */}Expense Form
+            {/* Expense Form */}
+            <ExpenseForm handleSubmit={handleSubmit} charge={charge} handleCharge={handleCharge} amount={amount} handleAmount={handleAmount} />
           </div>
 
           <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-            {/*Expense List */}Expense List
+            {/*Expense List */}
+            <ExpenseList initialExpenses={expenses} handleDelete={handleDelete} />
           </div>
 
           <div style={{display: 'flex', justifyContent: 'start', marginTop: '1rem'}}>
@@ -25,5 +67,6 @@ export default class App extends Component{
         </div>
       </main>
     )
-  }
 }
+
+export default App;
